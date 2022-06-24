39e853d04bace01b83635ae5a92bdfaa5c82cbee
Agata <kosekboom@gmail.com>
2022-06-24 15:38:21 +0700

    Add posibilty to display shows and add validation

--- src/app.js
+++ src/app.js
@@ -55,23 +55,46 @@ class TvSearch {
   };
 
   renderCards = (shows) => {
+    this.viewElems.showsWrapper.innerHTML = "";
     for (const { show } of shows) {
       this.createShowCard(show);
     }
   };
 
   createShowCard = (show) => {
+    let img;
+    let p;
     const divCard = createDOMElement("div", "card");
-    const img = createDOMElement(
-      "img",
-      "card-img-top",
-      null,
-      show.image.medium
-    );
+
     const divCardBody = createDOMElement("div", "card-body");
     const h5 = createDOMElement("h5", "card-title", show.name);
-    const p = createDOMElement("p", "card-text", show.summary);
+
     const btn = createDOMElement("button", "btn btn-primary", "Show Details");
+
+    if (show.image) {
+      img = createDOMElement("img", "card-img-top", null, show.image?.medium);
+    } else {
+      img = createDOMElement(
+        "img",
+        "card-img-top",
+        null,
+        "https://via.placeholder.com/210x295"
+      );
+    }
+
+    if (show.summary) {
+      p = createDOMElement(
+        "p",
+        "card-text",
+        `${show.summary.slice(0, 100)}...`
+      );
+    } else {
+      p = createDOMElement(
+        "p",
+        "card-text",
+        "There is no summary for this show yet"
+      );
+    }
 
     divCard.appendChild(divCardBody);
     divCardBody.appendChild(img);
