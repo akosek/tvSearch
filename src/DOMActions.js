06578a13fa8114aa9ba85524f57f956235738f25
Agata <kosekboom@gmail.com>
2022-06-24 15:22:27 +0700

    Display TV Content on Cards

--- src/DOMActions.js
+++ src/DOMActions.js
@@ -10,3 +10,13 @@ export const mapListToDOMElements = (listOfValues, attribute) => {
   }
   return _viewElem;
 };
+
+export const createDOMElement = (tagName, className, innerText, src) => {
+  const tag = document.createElement(tagName);
+  tag.classList = className;
+
+  if (innerText) tag.innerText = innerText;
+  if (src) tag.src = src;
+
+  return tag;
+};
--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { mapListToDOMElements } from "./DOMActions.js";
+import { mapListToDOMElements, createDOMElement } from "./DOMActions.js";
 import { getShowsByKey } from "./apiService.js";
 
 class TvSearch {
@@ -50,8 +50,36 @@ class TvSearch {
 
   fetchData = () => {
     getShowsByKey(this.selectedName).then((shows) => {
-      console.log("Found Show", shows);
+      this.renderCards(shows);
     });
+  };
+
+  renderCards = (shows) => {
+    for (const { show } of shows) {
+      this.createShowCard(show);
+    }
+  };
+
+  createShowCard = (show) => {
+    const divCard = createDOMElement("div", "card");
+    const img = createDOMElement(
+      "img",
+      "card-img-top",
+      null,
+      show.image.medium
+    );
+    const divCardBody = createDOMElement("div", "card-body");
+    const h5 = createDOMElement("h5", "card-title", show.name);
+    const p = createDOMElement("p", "card-text", show.summary);
+    const btn = createDOMElement("button", "btn btn-primary", "Show Details");
+
+    divCard.appendChild(divCardBody);
+    divCardBody.appendChild(img);
+    divCardBody.appendChild(h5);
+    divCardBody.appendChild(p);
+    divCardBody.appendChild(btn);
+
+    this.viewElems.showsWrapper.appendChild(divCard);
   };
 }
 
